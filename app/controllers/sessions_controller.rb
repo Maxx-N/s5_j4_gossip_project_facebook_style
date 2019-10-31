c8d0d35b4d2a7255cbb007c1a5d50de7116a6ab6
Maxx-N <maximenorval@gmail.com>
2019-10-31 13:17:15 +0100

    on peut se connecter (session)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    puts "$" * 50
-    puts params
     user = User.find_by(email: params[:user_email])
 
     # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe 
-    if user && user.authenticate(password: params[:user_password])
+    if user && user.authenticate(params[:user_password])
       session[:user_id] = user.id
       # redirige où tu veux, avec un flash ou pas
-
+      redirect_to '/'
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
   end
 
 end
