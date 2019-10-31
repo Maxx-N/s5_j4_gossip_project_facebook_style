6617705ad1384fbb45b0b29dea386d8788cc638c
Maxx-N <maximenorval@gmail.com>
2019-10-31 10:37:11 +0100

    password ajouté pour les users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
   belongs_to :city, optional: true
   has_many :gossips
   has_many :sent_messages, foreign_key: "sender_id", class_name: "PrivateMessage"
@@ -11,5 +13,8 @@ class User < ApplicationRecord
     presence: true,
     uniqueness: true,
     format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
-  
+  validates :password,
+    presence: true,
+    length: { minimum: 6 }
+
 end
