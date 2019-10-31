b7eda25f0a97e82f91716b01eede6da791e07dbd
Maxx-N <maximenorval@gmail.com>
2019-10-31 11:36:59 +0100

    vérification du mail et du mdp lors de la connexion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,20 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    puts "$" * 50
+    puts params
+    user = User.find_by(email: params[:user_email])
+
+    # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe 
+    if user && user.authenticate(password: params[:user_password])
+      session[:user_id] = user.id
+      # redirige où tu veux, avec un flash ou pas
+
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
 end
