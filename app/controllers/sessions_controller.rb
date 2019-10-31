ec76dcd17978cb167618c88c171c07d1e933ae04
Maxx-N <maximenorval@gmail.com>
2019-10-31 15:45:50 +0100

    seed mis à jour

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete(:user_id)
+    redirect_to 'new'
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,44 +6,52 @@
   City.destroy_all
   Gossip.destroy_all
 
+# On crée 10 villes aléatoirement, notamment grâce à la gem "Faker" :
+10.times do |index|
+  c = City.new
+  c.id = index + 1
+  c.name = Faker::Address.unique.city
+  c.zip_code = "#{rand(0..9)}#{rand(1..5)}#{Faker::Number.unique.between(from: 10, to: 88)}0"
+  c.save
+end
+
+# On affiche la liste des villes (en limitant le nombre de colonnes) :
+puts "\n"
+puts "10 villes ont été ajoutées :"
+puts "\n"
+tp City.all, :id, :name, :zip_code
+
 # On crée 10 utilisateurs aléatoirement, notamment grâce à la gem "Faker", entre 15 et 30 ans :
-  10.times do |index|
+  9.times do |index|
     u = User.new
     u.id = index + 1
     u.first_name = Faker::Name.first_name
     u.last_name = Faker::Name.last_name 
     u.email = Faker::Internet.unique.email
+    u.password = "#{rand(100000..999999)}"
     u.age = rand(15..30)
     u.description = Faker::Hacker.say_something_smart
+    u.city = City.all.find(rand(1..City.all.length))
     u.save
   end
+
+# On crée un 10e utilisateur qui a le prénom "anonymous"
+  u = User.new
+  u.id = User.all.length + 1
+  u.first_name = "anonymous"
+  u.last_name = Faker::Name.last_name 
+  u.email = Faker::Internet.unique.email
+  u.password = "#{rand(100000..999999)}"
+  u.age = rand(15..30)
+  u.description = Faker::Hacker.say_something_smart
+  u.city = City.all.find(rand(1..City.all.length))
+  u.save
 
 # On affiche la liste des utilisateurs (en limitant le nombre de colonnes) :
   puts "\n"
   puts "10 utilisateurs(trices) ont été ajouté(e)s :"
   puts "\n"
   tp User.all, :first_name, :last_name, :email, :age
-
-# On crée 10 villes aléatoirement, notamment grâce à la gem "Faker" :
-  10.times do |index|
-    c = City.new
-    c.id = index + 1
-    c.name = Faker::Address.unique.city
-    c.zip_code = "#{rand(0..9)}#{rand(1..5)}#{Faker::Number.unique.between(from: 10, to: 88)}0"
-    c.save
-  end
-
-# On affiche la liste des villes (en limitant le nombre de colonnes) :
-  puts "\n"
-  puts "10 villes ont été ajoutées :"
-  puts "\n"
-  tp City.all, :id, :name, :zip_code
-
-# On attribue aléatoirement une ville à un  utilisateur (plusieurs utilisateurs peuvent venir de la même ville) :
-  User.all.each do |u|
-    u.city = City.all[rand(0..9)]
-    u.save
-  end
 
 # On affiche chaque utilisateur et la ville d'où il vient :
   puts "\n"
